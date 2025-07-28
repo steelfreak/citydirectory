c00713cd88262ff5b9972ba1410492c741f02107
kinen key samson <kinenkey@gmail.com>
2025-07-28 20:57:20 +0300

    Website Data
    Updated the links of websites

--- Websites/websites-data.js
+++ Websites/websites-data.js
@@ -1,6 +1,7 @@
 // websites-data.js
 const websites = [
   { title: "Gulu", url: "https://en.wikipedia.org/wiki/Gulu", category: "Place" },
+  { title: "Gulu map", url: "https://mapcarta.com/Gulu", category: "Place" },
   { title: "Gulu Tours", url: "https://www.gulutours.com/", category: "tourservice" },
   { title: "UCB", url: "https://ucb.go.ug/gulu/", category: "tourservice" },
   { title: "Gulu Airport", url: "https://en.wikipedia.org/wiki/Gulu_Airport", category: "place" },
@@ -8,5 +9,15 @@ const websites = [
   { title: "Gulu College of Health Sciece", url: "https://gchs.ac.ug/", category: "College" },
   { title: "Gulu Highschool", url: "https://guluhighschool.ac.ug/", category: "highschool" },
   { title: "Gulu University", url: "https://gu.ac.ug/", category: "University" },
+  { title: "Building Stronger University", url: "https://www.bsu.gu.ac.ug/", category: "Education" },
+  { title: "Gulu University", url: "https://gurl.gu.ac.ug/", category: "Education" },
+  { title: "Uganda Live & Travel", url: "https://ugandaliveandtravel.com/gulu-uganda/", category: "Tours" },
+  { title: "Gulu Airfield", url: "https://caa.go.ug/gulu-airfield/", category: "Transport" },
+  { title: "World Embrace", url: "https://www.worldembrace.org/gcp", category: "Leisure" },
+  { title: "Student Hub", url: "https://studenthub.ug/institutions/8/gulu-university", category: "Education" },
+  { title: "Gulu Central Highschool", url: "https://gulucentralhighschool.ac.ug/", category: "highschool" },
+  { title: "Gov Info", url: "https://govinfohub.go.ug/index.php/gulu-university/", category: "Info" },
+  { title: "The Elephant", url: "https://theelephant.co.ug/", category: "Info" },
+  { title: "Uganda Travel Guide", url: "https://www.ugandatravelguide.com/gulu-district/", category: "Info" },
 
 ];
